1e9c48fd6dfb53d20f254c22e52cedaca4b79a89
Olivier <o.blaivie@coperbee.fr>
2022-10-17 12:43:14 +0200

    lazy loading

--- appFormation/src/app/app-routing.module.ts
+++ appFormation/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './core/page-home/page-home.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
-import { PageListClientsComponent } from './features/clients/page-list-clients/page-list-clients.component';
-import { PageListOrdersComponent } from './features/orders/page-list-orders/page-list-orders.component';
 
 const routes: Routes = [
   { path: 'home', component: PageHomeComponent },
-  { path: 'orders', component: PageListOrdersComponent },
-  { path: 'clients', component: PageListClientsComponent },
+  { path: 'orders', loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersModule) },
+  { path: 'clients', loadChildren: () => import('./features/clients/clients.module').then(m => m.ClientsModule) },
+  // { path: 'orders', component: PageListOrdersComponent },
+  // { path: 'clients', component: PageListClientsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
